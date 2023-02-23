47ee2f92c184ad400f5664c50dcc65cdbe3961a3
khloptsev <p.khloptsev@gmail.com>
2023-02-23 16:07:17 +1000

    add fetch user logic

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,34 @@ import { Header } from 'components/Header';
 import { Search } from 'components/Search';
 import { UserCard } from 'components/UserCard';
 
-import { user } from 'mock';
+import { defaultUser } from 'mock';
+import { GithubUser, GithubUserError, LocalUser } from 'types';
+import { isGithubUser } from 'utils/typeGuards';
+import { extractLocalUser } from 'utils/extract-user';
+
+const BASE_URL = 'https://api.github.com/users/';
 
 const App = () => {
+  const [user, setUser] = React.useState<LocalUser | null>(defaultUser);
+
+  const fetchUser = async (username: string) => {
+    const url = BASE_URL + username;
+    const response = await fetch(url);
+    const user = (await response.json()) as GithubUser | GithubUserError;
+
+    if (isGithubUser(user)) {
+      const localUser = extractLocalUser(user);
+      setUser(localUser);
+    } else {
+      setUser(null);
+    }
+  };
+
   return (
     <Container>
       <Header />
-      <Search
-        hasError
-        onSubmit={(text) => {
-          console.log(text);
-        }}
-      />
-      <UserCard {...user} />
+      <Search hasError={!user} onSubmit={fetchUser} />
+      {user && <UserCard {...user} />}
     </Container>
   );
 };
