c8e8285b2d6467399de90a5387678c21d91ffd3a
khloptsev <p.khloptsev@gmail.com>
2023-02-23 16:06:54 +1000

    add extractLocalUser, add typeguards

---
+++ src/utils/extract-user.ts
@@ -0,0 +1,17 @@
+import { GithubUser, LocalUser } from 'types';
+
+export const extractLocalUser = (user: GithubUser): LocalUser => ({
+  login: user.login,
+  id: user.id,
+  avatar: user.avatar_url,
+  name: user.name,
+  company: user.company,
+  blog: user.blog,
+  location: user.location,
+  bio: user.bio,
+  twitter: user.twitter_username,
+  repos: user.public_repos,
+  followers: user.followers,
+  following: user.following,
+  created: user.created_at,
+});
