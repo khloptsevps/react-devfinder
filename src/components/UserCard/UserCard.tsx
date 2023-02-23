a1fd3983394df5d0e7e6bbc3790a81e0d7005e7e
khloptsev <p.khloptsev@gmail.com>
2023-02-23 14:28:26 +1000

    add UserTitile, add Styles

--- src/components/UserCard/UserCard.tsx
+++ src/components/UserCard/UserCard.tsx
@@ -1,11 +1,17 @@
 import { UserStat } from 'components/UserStat';
 import styles from './UserCard.module.scss';
 import { LocalUser } from 'types';
+import { UserTitle } from 'components/UserTitle';
 
 interface UserCardProps extends LocalUser {}
 
 export const UserCard = (props: UserCardProps) => (
   <div className={styles.userCard}>
+    <img className={styles.avatar} src={props.avatar} alt={props.name} />
+    <UserTitle name={props.name} created={props.created} login={props.login} />
+    <p className={`${styles.bio}${props.bio ? '' : ` ${styles.empty}`}`}>
+      {props.bio ? props.bio : 'This profile has no bio'}
+    </p>
     <UserStat
       repos={props.repos}
       followers={props.followers}
